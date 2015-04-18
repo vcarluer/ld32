7a5cd93b7399e1e0f506f4df71e00a72631c782b
vcarluer <vcarluer@gmail.com>
2015-04-18 18:54:18 +0200

    lmms bell gimp

---
+++ belle/ld32/assets.js
@@ -0,0 +1,1 @@
+game.assets = { "fonts" : [  ], "images" : [ { "name" : "intro_bkg.png" }, { "name" : "Spaceman_thb.png" } ], "sounds" : [ { "name" : "intro_0.wav" } ], "subdirs" : { "fonts" : "", "images" : "", "sounds" : "" } }
