a4afdc35eceefc15bbfa444dbcacb90ad149a184
vcarluer <vcarluer@gmail.com>
2015-04-19 00:22:44 +0200

    bkg 3 4 + var correction

--- belle/ld32/assets.js
+++ belle/ld32/assets.js
@@ -1,1 +1,1 @@
-game.assets = { "fonts" : [  ], "images" : [ { "name" : "Spaceman_thb.png" }, { "name" : "intro_bkg2.png" }, { "name" : "intro_bkg.png" } ], "sounds" : [ { "name" : "wilhelm.wav" }, { "name" : "intro_0.wav" } ], "subdirs" : { "fonts" : "", "images" : "", "sounds" : "" } }
+game.assets = { "fonts" : [  ], "images" : [ { "name" : "intro_bkg.png" }, { "name" : "Spaceman_thb.png" }, { "name" : "intro_bkg2.png" } ], "sounds" : [ { "name" : "wilhelm.wav" }, { "name" : "intro_0.wav" } ], "subdirs" : { "fonts" : "", "images" : "", "sounds" : "" } }
