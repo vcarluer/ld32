f1a0ed564f0b6ace6b818e2da51811ab3690c909
vcarluer <vcarluer@gmail.com>
2015-04-18 21:20:25 +0200

    intro 2

--- belle/ld32/assets.js
+++ belle/ld32/assets.js
@@ -1,1 +1,1 @@
-game.assets = { "fonts" : [  ], "images" : [ { "name" : "Spaceman_thb.png" }, { "name" : "intro_bkg.png" } ], "sounds" : [ { "name" : "intro_0.wav" } ], "subdirs" : { "fonts" : "", "images" : "", "sounds" : "" } }
+game.assets = { "fonts" : [  ], "images" : [ { "name" : "Spaceman_thb.png" }, { "name" : "intro_bkg2.png" }, { "name" : "intro_bkg.png" } ], "sounds" : [ { "name" : "wilhelm.wav" }, { "name" : "intro_0.wav" } ], "subdirs" : { "fonts" : "", "images" : "", "sounds" : "" } }
